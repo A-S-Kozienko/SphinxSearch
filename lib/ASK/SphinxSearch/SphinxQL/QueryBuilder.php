24f725abcf9a3f5a06836a96fa4679b695b3f39a
A.S.Kozienko <a.s.kozienko@gmail.com>
2013-02-02 21:29:44 +0200

    QueryBuilder

--- lib/ASK/SphinxSearch/SphinxQL/QueryBuilder.php
+++ lib/ASK/SphinxSearch/SphinxQL/QueryBuilder.php
@@ -1,8 +1,29 @@
 <?php
 namespace ASK\SphinxSearch\SphinxQL;
 
+/**
+ * QueryBuilder
+ */
 class QueryBuilder
 {
+    const ORDER_ASC                                         = 'ASC';
+
+    const ORDER_DESC                                        = 'DESC';
+
+    const RANKER_PROXIMITY_BM25                             = 'proximity_bm25';
+
+    const RANKER_BM25                                       = 'bm25';
+
+    const RANKER_NONE                                       = 'none';
+
+    const RANKER_WORD_COUNT                                 = 'wordcount';
+
+    const RANKER_PROXIMITY                                  = 'proximity';
+
+    const RANKER_MATCH_ANY                                  = 'matchany';
+
+    const RANKER_FIELD_MASK                                 = 'fieldmask';
+
     /**
      * @var array
      */
@@ -23,6 +44,46 @@ class QueryBuilder
      */
     protected $parameters = array();
 
+    /**
+     * @var string
+     */
+    protected $groupBy;
+
+    /**
+     * @var array
+     */
+    protected $orderBy = array();
+
+    /**
+     * @var array
+     */
+    protected $withinGroupOrderBy;
+
+    /**
+     * @var array
+     */
+    protected $limit;
+
+    /**
+     * @var array
+     */
+    protected $options = array();
+
+    /**
+     * @var array
+     */
+    protected $optionsList = array();
+
+    /**
+     * @var string
+     */
+    protected $comment;
+
+    /**
+     * @param string $expr
+     *
+     * @return $this
+     */
     public function select($expr)
     {
         $this->select = array($expr);
@@ -30,6 +91,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $expr
+     *
+     * @return $this
+     */
     public function addSelect($expr)
     {
         $this->select[] = $expr;
@@ -37,6 +103,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $index
+     *
+     * @return $this
+     */
     public function from($index)
     {
         $this->from = array($index);
@@ -44,6 +115,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $index
+     *
+     * @return $this
+     */
     public function addFrom($index)
     {
         $this->from[] = $index;
@@ -51,6 +127,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $condition
+     *
+     * @return $this
+     */
     public function where($condition)
     {
         $this->where = array($condition);
@@ -58,6 +139,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $condition
+     *
+     * @return $this
+     */
     public function addWhere($condition)
     {
         $this->where[] = $condition;
@@ -65,6 +151,11 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param array $parameters
+     *
+     * @return $this
+     */
     public function setParameters(array $parameters)
     {
         $this->parameters = $parameters;
@@ -72,6 +163,12 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $key
+     * @param string $value
+     *
+     * @return $this
+     */
     public function setParameter($key, $value)
     {
         $this->parameters[$key] = $value;
@@ -79,6 +176,228 @@ class QueryBuilder
         return $this;
     }
 
+    /**
+     * @param string $column
+     *
+     * @return $this
+     */
+    public function groupBy($column)
+    {
+        $this->groupBy = $column;
+
+        return $this;
+    }
+
+    /**
+     * @param string $column
+     * @param string $order
+     *
+     * @return $this
+     */
+    public function orderBy($column, $order = self::ORDER_ASC)
+    {
+        $this->orderBy = array(array($column, $order));
+
+        return $this;
+    }
+
+    /**
+     * @param string $column
+     * @param string $order
+     *
+     * @return $this
+     */
+    public function addOrderBy($column, $order = self::ORDER_ASC)
+    {
+        $this->orderBy[] = array($column, $order);
+
+        return $this;
+    }
+
+    /**
+     * @param string $column
+     * @param string $order
+     *
+     * @return $this
+     */
+    public function withinGroupOrderBy($column, $order = self::ORDER_ASC)
+    {
+        $this->withinGroupOrderBy = array($column, $order);
+
+        return $this;
+    }
+
+    /**
+     * @param int $count
+     * @param int $offset
+     *
+     * @return $this
+     */
+    public function limit($count, $offset = 0)
+    {
+        $this->limit = array((int) $offset, (int) $count);
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return $this
+     */
+    public function setRanker($value)
+    {
+        $this->options['ranker'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setMaxMatches($value)
+    {
+        $this->options['max_matches'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setCutoff($value)
+    {
+        $this->options['cutoff'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setMaxQueryTime($value)
+    {
+        $this->options['max_query_time'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setRetryCount($value)
+    {
+        $this->options['retry_count'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     *
+     * @return $this
+     */
+    public function setRetryDelay($value)
+    {
+        $this->options['retry_delay'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $reverseScan
+     *
+     * @return $this
+     */
+    public function setReverseScan($reverseScan)
+    {
+        $this->options['reverse_scan'] = $reverseScan ? 1 : 0;
+
+        return $this;
+    }
+
+    /**
+     * @param array $values
+     *
+     * @return $this
+     */
+    public function setFieldWeights(array $values)
+    {
+        $this->optionsList['field_weights'] = $values;
+
+        return $this;
+    }
+
+    /**
+     * @param string $field
+     * @param int    $weight
+     *
+     * @return $this
+     */
+    public function addFieldWeight($field, $weight)
+    {
+        if (false == isset($this->optionsList['field_weights'])) {
+            $this->optionsList['field_weights'] = array();
+        }
+
+        $this->optionsList['field_weights'][$field] = $weight;
+
+        return $this;
+    }
+
+    /**
+     * @param array $values
+     *
+     * @return $this
+     */
+    public function setIndexWeights(array $values)
+    {
+        $this->optionsList['index_weights'] = $values;
+
+        return $this;
+    }
+
+    /**
+     * @param string $index
+     * @param int    $weight
+     *
+     * @return $this
+     */
+    public function addIndexWeight($index, $weight)
+    {
+        if (false == isset($this->optionsList['index_weights'])) {
+            $this->optionsList['index_weights'] = array();
+        }
+
+        $this->optionsList['index_weights'][$index] = $weight;
+
+        return $this;
+    }
+
+    /**
+     * @param string $comment
+     *
+     * @return $this
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     * @throws \InvalidArgumentException
+     */
     public function getSphinxQL()
     {
         if (false == $this->select) {
@@ -94,6 +413,54 @@ class QueryBuilder
 
         if ($this->where) {
             $sphinxQl .= ' WHERE ' . implode(' ', $this->where);
+        }
+
+        if ($this->groupBy) {
+            $sphinxQl .= ' GROUP BY ' . $this->groupBy;
+        }
+
+        if ($this->orderBy) {
+            $options = array();
+            foreach ($this->orderBy as $orderBy) {
+                $options[] .= $orderBy[0] . ' ' . $orderBy[1];
+            }
+
+            $sphinxQl .= ' ORDER BY ' . implode(', ', $options);
+        }
+
+        if ($this->withinGroupOrderBy) {
+            $sphinxQl .= ' WITHIN GROUP ORDER BY ' . implode(' ', $this->withinGroupOrderBy);
+        }
+
+        if ($this->limit) {
+            $sphinxQl .= ' LIMIT ' . implode(', ', $this->limit);
+        }
+
+        $options = array();
+
+        if ($this->options) {
+            foreach ($this->options as $name => $value) {
+                $options[] = $name . '=' . $value;
+            }
+        }
+
+        if ($this->optionsList) {
+            foreach ($this->optionsList as $optionName => $optionValue) {
+                $weights = array();
+                foreach ($optionValue as $field => $weight) {
+                    $weights[] = $field . '=' . $weight;
+                }
+
+                $options[] = $optionName . '=(' . implode(', ', $weights) . ')';
+            }
+        }
+
+        if ($this->comment) {
+            $options[] = "comment='" . str_replace("'", "\\'", $this->comment) . "'";
+        }
+
+        if ($options) {
+            $sphinxQl .= ' OPTION ' . implode(', ', $options);
         }
 
         return $sphinxQl;
--- tests/ASK/SphinxSearch/Tests/SphinxQL/QueryBuilderTest.php
+++ tests/ASK/SphinxSearch/Tests/SphinxQL/QueryBuilderTest.php
@@ -1,9 +1,11 @@
 <?php
 namespace ASK\SphinxSearch\Tests\SphinxQL;
 
-
 use ASK\SphinxSearch\SphinxQL\QueryBuilder;
 
+/**
+ * QueryBuilderTest
+ */
 class QueryBuilderTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -52,14 +54,274 @@ class QueryBuilderTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function shouldReturnSphinxQLWithWhere()
+    public function shouldSetWhere()
     {
         $qb = new QueryBuilder();
         $qb->select('expr')
             ->from('index')
-            ->where('condition')
-        ;
+            ->where('condition');
 
         $this->assertEquals('SELECT expr FROM index WHERE condition', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetGroupBy()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->groupBy('group_by');
+
+        $this->assertEquals('SELECT expr FROM index GROUP BY group_by', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetSingleOrderBy()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->orderBy('column', 'DESC');
+
+        $this->assertEquals('SELECT expr FROM index ORDER BY column DESC', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetMultipleOrderBy()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->orderBy('column', 'DESC')
+            ->addOrderBy('column2', 'ASC');
+
+        $this->assertEquals('SELECT expr FROM index ORDER BY column DESC, column2 ASC', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetWithinGroupOrderBy()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->withinGroupOrderBy('column', 'DESC');
+
+        $this->assertEquals('SELECT expr FROM index WITHIN GROUP ORDER BY column DESC', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetLimitWithoutOffset()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->limit(10);
+
+        $this->assertEquals('SELECT expr FROM index LIMIT 0, 10', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetLimitWithOffset()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->limit(10, 100);
+
+        $this->assertEquals('SELECT expr FROM index LIMIT 100, 10', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionRanker()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setRanker(QueryBuilder::RANKER_BM25);
+
+        $this->assertEquals('SELECT expr FROM index OPTION ranker=bm25', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionMaxMatches()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setMaxMatches(100);
+
+        $this->assertEquals('SELECT expr FROM index OPTION max_matches=100', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionCutoff()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setCutoff(100);
+
+        $this->assertEquals('SELECT expr FROM index OPTION cutoff=100', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionMaxQueryTime()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setMaxQueryTime(100);
+
+        $this->assertEquals('SELECT expr FROM index OPTION max_query_time=100', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionRetryCount()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setRetryCount(100);
+
+        $this->assertEquals('SELECT expr FROM index OPTION retry_count=100', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionRetryDelay()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setRetryDelay(100);
+
+        $this->assertEquals('SELECT expr FROM index OPTION retry_delay=100', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionFieldWeights()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setFieldWeights(array(
+                'field'  => 1,
+                'field2' => 2,
+            ));
+
+        $this->assertEquals('SELECT expr FROM index OPTION field_weights=(field=1, field2=2)', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAddOptionFieldWeight()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->addFieldWeight('field', 1)
+            ->addFieldWeight('field2', 2);
+
+        $this->assertEquals('SELECT expr FROM index OPTION field_weights=(field=1, field2=2)', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionIndexWeights()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setIndexWeights(array(
+                'field'  => 1,
+                'field2' => 2,
+            ));
+
+        $this->assertEquals('SELECT expr FROM index OPTION index_weights=(field=1, field2=2)', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAddOptionIndexWeight()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->addIndexWeight('field', 1)
+            ->addIndexWeight('field2', 2);
+
+        $this->assertEquals('SELECT expr FROM index OPTION index_weights=(field=1, field2=2)', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionReverseScan()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setReverseScan(true);
+
+        $this->assertEquals('SELECT expr FROM index OPTION reverse_scan=1', $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetOptionComment()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setComment("comment'comment");
+
+        $this->assertEquals("SELECT expr FROM index OPTION comment='comment\'comment'", $qb->getSphinxQL());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetMultiplyOptions()
+    {
+        $qb = new QueryBuilder();
+        $qb->select('expr')
+            ->from('index')
+            ->setRetryCount(1)
+            ->setRetryDelay(2)
+            ->setFieldWeights(array(
+                'field' => 3,
+                'field2' => 4,
+            ));
+
+        $this->assertEquals("SELECT expr FROM index OPTION retry_count=1, retry_delay=2, field_weights=(field=3, field2=4)", $qb->getSphinxQL());
     }
 }
